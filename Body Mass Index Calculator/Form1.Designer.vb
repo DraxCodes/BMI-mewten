a709f49c8ea770e2322a7168aafe2c3989a2fbeb
joelp53 <joelparkinson@live.co.uk>
2015-05-08 18:20:28 +0100

    Added 'BMI Calculator' to Repo

---
+++ Body Mass Index Calculator/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBMI
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblBMI = New System.Windows.Forms.Label
+        Me.lblWeight = New System.Windows.Forms.Label
+        Me.lblHeight = New System.Windows.Forms.Label
+        Me.txtWeight = New System.Windows.Forms.TextBox
+        Me.txtHeight = New System.Windows.Forms.TextBox
+        Me.lblAnswer = New System.Windows.Forms.Label
+        Me.btnCalculate = New System.Windows.Forms.Button
+        Me.pbResult = New System.Windows.Forms.PictureBox
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        CType(Me.pbResult, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblBMI
+        '
+        Me.lblBMI.AutoSize = True
+        Me.lblBMI.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBMI.Location = New System.Drawing.Point(26, 37)
+        Me.lblBMI.Name = "lblBMI"
+        Me.lblBMI.Size = New System.Drawing.Size(439, 37)
+        Me.lblBMI.TabIndex = 0
+        Me.lblBMI.Text = "Body Mass Index Calculator"
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                        Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeight.Location = New System.Drawing.Point(28, 103)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(143, 25)
+        Me.lblWeight.TabIndex = 1
+        Me.lblWeight.Text = "Weight in kg"
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeight.Location = New System.Drawing.Point(30, 222)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(184, 25)
+        Me.lblHeight.TabIndex = 2
+        Me.lblHeight.Text = "Height in metres"
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(33, 144)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(105, 20)
+        Me.txtWeight.TabIndex = 3
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(33, 255)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(105, 20)
+        Me.txtHeight.TabIndex = 4
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(328, 112)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(39, 13)
+        Me.lblAnswer.TabIndex = 5
+        Me.lblAnswer.Text = "Label4"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(33, 318)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(115, 38)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'pbResult
+        '
+        Me.pbResult.Location = New System.Drawing.Point(253, 199)
+        Me.pbResult.Name = "pbResult"
+        Me.pbResult.Size = New System.Drawing.Size(211, 156)
+        Me.pbResult.TabIndex = 8
+        Me.pbResult.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.QuitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(514, 24)
+        Me.MenuStrip1.TabIndex = 9
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(42, 20)
+        Me.QuitToolStripMenuItem.Text = "Quit"
+        '
+        'frmBMI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(514, 402)
+        Me.Controls.Add(Me.pbResult)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.lblBMI)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblWeight)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmBMI"
+        Me.Text = "Body Mass Index"
+        CType(Me.pbResult, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblBMI As System.Windows.Forms.Label
+    Friend WithEvents lblWeight As System.Windows.Forms.Label
+    Friend WithEvents lblHeight As System.Windows.Forms.Label
+    Friend WithEvents txtWeight As System.Windows.Forms.TextBox
+    Friend WithEvents txtHeight As System.Windows.Forms.TextBox
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents pbResult As System.Windows.Forms.PictureBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents QuitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Body Mass Index Calculator/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class frmBMI
+    Dim info As String = "This is the Body Mass Index Calculator created by: Dustin Lopez"
+    Dim checker As Int32 = 0
+
+#Region "Minimum/Maximum Body Mass"
+    'Grossly Undernourished
+    Dim GUndernourished As Int32 = 16
+    'Min Undernourished
+    Dim minUndernourished As Int32 = 17
+    'Max Undernourished
+    Dim maxUndernourished As Int32 = 19
+    'Min Perfectly OK
+    Dim minPerfect As Int32 = 20
+    'Max Perfectly OK
+    Dim maxPerfect As Int32 = 25
+    'Min Fat
+    Dim minFat As Int32 = 26
+    'Max Fat
+    Dim maxFat As Int32 = 29
+    'Min Obese
+    Dim minObese As Int32 = 30
+    'Max Obese
+    Dim maxObese As Int32 = 39
+    'Very Obese
+    Dim VeryObese As Int32 = 40
+
+#End Region
+
+#Region "Button calculate click action"
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        check()
+    End Sub
+#End Region
+
+#Region "calculation and checking"
+    Dim result, remaining As Int32
+    Private Sub check()
+        If Val(txtWeight.Text) < 10 Then
+            lblAnswer.Text = "lower than 10"
+        ElseIf Val(txtWeight.Text) > 600 Then
+            lblAnswer.Text = "Weight number to large"
+        Else
+            result = Calc(Val(txtWeight.Text), Val(txtHeight.Text))
+
+            Select Case result
+
+                Case GUndernourished
+
+                Case minUndernourished To maxUndernourished
+
+                Case minPerfect To maxPerfect
+
+                Case minFat To maxFat
+
+                Case minObese To maxObese
+
+                Case VeryObese
+
+            End Select
+        End If
+
+    End Sub
+#End Region
+
+    Function Calc(ByVal w As Int32, ByVal h As Int32)
+        Return w / (h * h)
+    End Function
+
+    Private Sub QuitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles QuitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
